0a2197731078f1595f914754d4d3b8ba28048beb
coldfood <yuyangcoding@gmail.com>
2025-04-07 22:38:15 +0800

    update
    后端部分更新

--- core/handler/qweather_request.go
+++ core/handler/qweather_request.go
@@ -7,23 +7,24 @@ import (
 	"core/request"
 	"encoding/json"
 	"fmt"
+	"github.com/mitchellh/mapstructure"
 	"io"
 	"net/http"
 )
 
-func QueryForPositionWithLonLat(position *request.QueryWeatherLonLatRequest) (map[string]interface{}, error) {
+func QueryForPositionWithLonLat(position *request.QueryWeatherLonLatRequest) (Position, error) {
 	strLon, strLat := position.Longitude, position.Latitude
 	queryCurPosUrl := config.UrbanSearchAPI + strLon + "," + strLat
 	req, err := http.NewRequest("GET", queryCurPosUrl, nil)
 	if err != nil {
-		return nil, err
+		return Position{}, err
 	}
 	req.Header.Set(config.APIKeyHeader, config.ApiKey)
 	// 发起请求
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return nil, err
+		return Position{}, err
 	}
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
@@ -33,15 +34,14 @@ func QueryForPositionWithLonLat(position *request.QueryWeatherLonLatRequest) (ma
 	}(resp.Body)
 	// 解析响应
 	if resp.StatusCode != http.StatusOK {
-		// 修复：打印状态码和请求 URL
 		fmt.Printf("Request failed with status code: %d, URL: %s\n", resp.StatusCode, resp.Request.URL.String())
-		return nil, err
+		return Position{}, constant.NewError(-1, constant.CannotQueryPositionInfo, "无法查询到位置信息")
 	}
-	var reader io.ReadCloser = resp.Body
+	var reader = resp.Body
 	if resp.Header.Get("Content-Encoding") == "gzip" {
 		reader, err = gzip.NewReader(resp.Body)
 		if err != nil {
-			return nil, err
+			return Position{}, err
 		}
 		defer func(reader io.ReadCloser) {
 			err := reader.Close()
@@ -50,42 +50,45 @@ func QueryForPositionWithLonLat(position *request.QueryWeatherLonLatRequest) (ma
 			}
 		}(reader)
 	}
-
 	var response map[string]interface{}
 	if err := json.NewDecoder(reader).Decode(&response); err != nil {
-		return nil, err
+		return Position{}, err
 	}
 	// 提取第一个位置信息
 	locationInterface, ok := response["location"]
 	if !ok {
-		return nil, constant.NewError(-1, constant.CannotQueryPositionInfo, "无法解析位置信息")
+		return Position{}, constant.NewError(-1, constant.CannotQueryPositionInfo, "无法解析位置信息")
 	}
 	locations, ok := locationInterface.([]interface{})
 	if !ok {
-		return nil, constant.NewError(-1, constant.CannotQueryPositionInfo, "无法解析位置信息")
+		return Position{}, constant.NewError(-1, constant.CannotQueryPositionInfo, "无法解析位置信息")
 	}
 	if len(locations) == 0 {
-		return nil, constant.NewError(-1, constant.CannotQueryPositionInfo, "无法解析位置信息")
+		return Position{}, constant.NewError(-1, constant.CannotQueryPositionInfo, "无法解析位置信息")
 	}
 	firstLocationInterface, ok := locations[0].(map[string]interface{})
 	if !ok {
-		return nil, constant.NewError(-1, constant.CannotQueryPositionInfo, "无法解析位置信息")
+		return Position{}, constant.NewError(-1, constant.CannotQueryPositionInfo, "无法解析位置信息")
 	}
-	return firstLocationInterface, nil
+	var pos Position
+	if err := mapstructure.Decode(firstLocationInterface, &pos); err != nil {
+		return Position{}, err
+	}
+	return pos, nil
 }
 
-func QueryForNowWeather(position *request.QueryWeatherLonLatRequest) (map[string]interface{}, error) {
+func QueryForNowWeather(position *request.QueryWeatherLonLatRequest) (Weather, error) {
 	strLon, strLat := position.Longitude, position.Latitude
 	queryNowWeatherUrl := config.RealTimeWeatherQueryAPI + strLon + "," + strLat
 	req, err := http.NewRequest("GET", queryNowWeatherUrl, nil)
 	if err != nil {
-		return nil, err
+		return Weather{}, err
 	}
 	req.Header.Set(config.APIKeyHeader, config.ApiKey)
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return nil, err
+		return Weather{}, err
 	}
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
@@ -94,36 +97,53 @@ func QueryForNowWeather(position *request.QueryWeatherLonLatRequest) (map[string
 		}
 	}(resp.Body)
 	if resp.StatusCode != http.StatusOK {
-		return nil, constant.NewError(-1, constant.CannotQueryWeatherInfo, "无法查询到天气信息")
+		return Weather{}, constant.NewError(-1, constant.CannotQueryWeatherInfo, "无法查询到天气信息")
+	}
+	var reader = resp.Body
+	if resp.Header.Get("Content-Encoding") == "gzip" {
+		reader, err = gzip.NewReader(resp.Body)
+		if err != nil {
+			return Weather{}, err
+		}
+		defer func(reader io.ReadCloser) {
+			err := reader.Close()
+			if err != nil {
+				return
+			}
+		}(reader)
 	}
 	var response map[string]interface{}
-	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
-		return nil, err
+	if err := json.NewDecoder(reader).Decode(&response); err != nil {
+		return Weather{}, err
 	}
 	// 提取now
 	nowInterface, ok := response["now"]
 	if !ok {
-		return nil, constant.NewError(-1, constant.CannotQueryWeatherInfo, "无法查询到天气信息")
+		return Weather{}, constant.NewError(-1, constant.CannotQueryWeatherInfo, "无法查询到天气信息")
 	}
 	now, ok := nowInterface.(map[string]interface{})
 	if !ok {
-		return nil, constant.NewError(-1, constant.CannotQueryWeatherInfo, "无法查询到天气信息")
+		return Weather{}, constant.NewError(-1, constant.CannotQueryWeatherInfo, "无法查询到天气信息")
 	}
-	return now, nil
+	var weather Weather
+	if err := mapstructure.Decode(now, &weather); err != nil {
+		return Weather{}, err
+	}
+	return weather, nil
 }
 
-func QueryAirQuality(position *request.QueryWeatherLonLatRequest) (map[string]interface{}, error) {
+func QueryAirQuality(position *request.QueryWeatherLonLatRequest) (AirQuality, error) {
 	strLon, strLat := position.Longitude, position.Latitude
 	queryAirQualityUrl := config.AirQualityQueryAPI + strLat + "/" + strLon
 	req, err := http.NewRequest("GET", queryAirQualityUrl, nil)
 	if err != nil {
-		return nil, err
+		return AirQuality{}, err
 	}
 	req.Header.Set(config.APIKeyHeader, config.ApiKey)
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return nil, err
+		return AirQuality{}, err
 	}
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
@@ -133,27 +153,44 @@ func QueryAirQuality(position *request.QueryWeatherLonLatRequest) (map[string]in
 	}(resp.Body)
 	if resp.StatusCode != http.StatusOK {
 		fmt.Printf("Request failed with status code: %d, URL: %s\n", resp.StatusCode, resp.Request.URL.String())
-		return nil, constant.NewError(-1, constant.CannotQueryAirQualityInfo, "无法查询到天气信息")
+		return AirQuality{}, constant.NewError(-1, constant.CannotQueryAirQualityInfo, "无法查询到天气信息")
+	}
+	var reader = resp.Body
+	if resp.Header.Get("Content-Encoding") == "gzip" {
+		reader, err = gzip.NewReader(resp.Body)
+		if err != nil {
+			return AirQuality{}, err
+		}
+		defer func(reader io.ReadCloser) {
+			err := reader.Close()
+			if err != nil {
+				return
+			}
+		}(reader)
 	}
 	var response map[string]interface{}
-	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
-		return nil, err
+	if err := json.NewDecoder(reader).Decode(&response); err != nil {
+		return AirQuality{}, err
 	}
 	// 提取 indexes
 	indexesInterface, ok := response["indexes"]
 	if !ok {
-		return nil, constant.NewError(-1, constant.CannotQueryAirQualityInfo, "无法查询到天气信息")
+		return AirQuality{}, constant.NewError(-1, constant.CannotQueryAirQualityInfo, "无法查询到天气信息")
 	}
 	indexes, ok := indexesInterface.([]interface{})
 	if !ok {
-		return nil, constant.NewError(-1, constant.CannotQueryAirQualityInfo, "无法查询到天气信息")
+		return AirQuality{}, constant.NewError(-1, constant.CannotQueryAirQualityInfo, "无法查询到天气信息")
 	}
 	if len(indexes) == 0 {
-		return nil, constant.NewError(-1, constant.CannotQueryAirQualityInfo, "无法查询到天气信息")
+		return AirQuality{}, constant.NewError(-1, constant.CannotQueryAirQualityInfo, "无法查询到天气信息")
 	}
 	firstIndexInterface, ok := indexes[0].(map[string]interface{})
 	if !ok {
-		return nil, constant.NewError(-1, constant.CannotQueryAirQualityInfo, "无法查询到天气信息")
+		return AirQuality{}, constant.NewError(-1, constant.CannotQueryAirQualityInfo, "无法查询到天气信息")
 	}
-	return firstIndexInterface, nil
+	var airQuality AirQuality
+	if err := mapstructure.Decode(firstIndexInterface, &airQuality); err != nil {
+		return AirQuality{}, err
+	}
+	return airQuality, nil
 }
--- core/handler/weather_lon_lat.go
+++ core/handler/weather_lon_lat.go
@@ -6,6 +6,7 @@ import (
 	"core/model"
 	"core/request"
 	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"math"
 	"net/http"
@@ -15,7 +16,7 @@ import (
 
 func QueryWeatherByLonLat(c *gin.Context) {
 	// 解析请求坐标
-	var position *request.QueryWeatherLonLatRequest
+	var position request.QueryWeatherLonLatRequest
 	if err := c.ShouldBind(&position); err != nil {
 		ResponseFail(c, http.StatusBadRequest, constant.DataParseError, err.Error())
 		return
@@ -37,26 +38,26 @@ func QueryWeatherByLonLat(c *gin.Context) {
 	position.Longitude = strconv.FormatFloat(math.Trunc(longitude*100)/100, 'f', 2, 64)
 	position.Latitude = strconv.FormatFloat(math.Trunc(latitude*100)/100, 'f', 2, 64)
 	// 向和风天气发起调用
-	pos, err := QueryForPositionWithLonLat(position)
+	pos, err := QueryForPositionWithLonLat(&position)
 	if err != nil {
 		ResponseFail(c, http.StatusInternalServerError, constant.CannotQueryPositionInfo, err.Error())
 		return
 	}
-	weather, err := QueryForNowWeather(position)
+	weather, err := QueryForNowWeather(&position)
 	if err != nil {
 		ResponseFail(c, http.StatusInternalServerError, constant.CannotQueryWeatherInfo, err.Error())
 		return
 	}
-	airQuality, err := QueryAirQuality(position)
+	airQuality, err := QueryAirQuality(&position)
 	if err != nil {
 		ResponseFail(c, http.StatusInternalServerError, constant.CannotQueryAirQualityInfo, err.Error())
 		return
 	}
-	city, ok := pos["name"].(string)
-	if !ok {
-		ResponseFail(c, http.StatusInternalServerError, constant.CannotQueryPositionInfo, "无法查询到城市名称")
-		return
-	}
+	city := pos.Name
+	//if !ok {
+	//	ResponseFail(c, http.StatusInternalServerError, constant.CannotQueryPositionInfo, "无法查询到城市名称")
+	//	return
+	//}
 	// 将 weather 转换为 JSON 字符串
 	weatherJSON, err := json.Marshal(weather)
 	if err != nil {
@@ -77,10 +78,92 @@ func QueryWeatherByLonLat(c *gin.Context) {
 		ResponseFail(c, http.StatusInternalServerError, constant.DataBaseSaveError, err.Error())
 		return
 	}
-	data := map[string]interface{}{
-		"pos":         pos,
-		"weather":     weather,
-		"air_quality": airQuality,
+	fmt.Printf("pos: %+v\n", pos)
+	fmt.Printf("weather: %+v\n", weather)
+	fmt.Printf("airQuality: %+v\n\n", airQuality)
+	var data = Data{
+		Pos:        pos,
+		Weather:    weather,
+		AirQuality: airQuality,
 	}
 	ResponseSuccessWithData(c, data)
+}
+
+// 定义返回的数据结构
+type ResponseData struct {
+	Data    Data   `json:"data"`
+	Message string `json:"message"`
+}
+
+type Data struct {
+	AirQuality AirQuality `json:"air_quality"`
+	Pos        Position   `json:"pos"`
+	Weather    Weather    `json:"weather"`
+}
+
+type AirQuality struct {
+	Aqi              int       `json:"aqi"`
+	AqiDisplay       string    `json:"aqiDisplay"`
+	Category         string    `json:"category"`
+	Code             string    `json:"code"`
+	Color            Color     `json:"color"`
+	Health           Health    `json:"health"`
+	PrimaryPollutant Pollutant `json:"primaryPollutant"`
+}
+
+type Color struct {
+	Alpha float64 `json:"alpha"`
+	Blue  int     `json:"blue"`
+	Green int     `json:"green"`
+	Red   int     `json:"red"`
+}
+
+type Health struct {
+	Advice Advice `json:"advice"`
+	Level  string `json:"level"`
+	Name   string `json:"name"`
+}
+
+type Advice struct {
+	GeneralPopulation   string `json:"generalPopulation"`
+	SensitivePopulation string `json:"sensitivePopulation"`
+	Effect              string `json:"effect"`
+}
+
+type Pollutant struct {
+	Code     string `json:"code"`
+	FullName string `json:"fullName"`
+	Name     string `json:"name"`
+}
+
+type Position struct {
+	Adm1      string `json:"adm1"`
+	Aweather  string `json:"aweather.com/weather/chang'an-101110102.html"`
+	Id        string `json:"id"`
+	IsDst     string `json:"isDst"`
+	Lat       string `json:"lat"`
+	Lon       string `json:"lon"`
+	Name      string `json:"name"`
+	Rank      string `json:"rank"`
+	Type      string `json:"type"`
+	Tz        string `json:"tz"`
+	UtcOffset string `json:"utcOffset"`
+}
+
+type Weather struct {
+	Cloud           string `json:"cloud"`
+	Dew             string `json:"dew"`
+	FlsLike         string `json:"flsLike"`
+	Humidity        string `json:"humidity"`
+	Icon            string `json:"icon"`
+	ObservationTime string `json:"obsTime"`
+	Precip          string `json:"precip"`
+	Pressure        string `json:"pressure"`
+	Temp            string `json:"temp"`
+	Text            string `json:"text"`
+	Vis             string `json:"vis"`
+	Wind360         string `json:"wind360"`
+	WindDir         string `json:"windDir"`
+	WindScale       string `json:"windScale"`
+	WindSpeed       string `json:"windSpeed"`
 }
