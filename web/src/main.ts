64c87d2d24cd5bdacbe8f2ecdfde79d864c50b48
lhx-666-cool <d3032471065@yeah.net>
2025-04-10 18:09:49 +0800

    add pwd application

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,10 +3,12 @@ import './style.css'
 import App from './App.vue'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'qweather-icons/font/qweather-icons.css'
+
+
 const app = createApp(App)
 
 app.mount('#app')
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
-  }
+  app.component(key, component)
+}
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,7 +16,35 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()],
-    }),],
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      manifest: {
+        name: 'Haze Detection',
+        short_name: 'Haze Detection',
+        description: 'Haze Detection',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'image-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'image-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      workbox: {},
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
   server: {
     host: '0.0.0.0'
   }
